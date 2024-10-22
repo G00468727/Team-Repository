5d802c5b484a8e89536331d26248abd6a5aa21d5
G00465926 <G00465926@atu.ie>
2024-10-22 15:45:49 +0100

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,6 @@
 // for github lab 2
 public class Welcome{
  public static void main(String[] args){
- System.out.println("Oisin is here and ready to party!");
+ System.out.println("Oisin is here and ready to party!!");
    }
  }
